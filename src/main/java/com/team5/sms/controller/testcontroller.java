62a68055fa28dab445b5a99014a47532947cc931
starnightx7 <starnightx7@.163.com>
2018-10-26 17:52:50 +0800

    添加test

---
+++ src/main/java/com/team5/sms/controller/testcontroller.java
@@ -0,0 +1,12 @@
+package com.team5.sms.controller;
+
+/**
+ * All rights Reserved, Designed By Suixingpay.
+ *
+ * @author: wangzhiyuan[wang_zy1@suixingpay.com]
+ * @date: 2018年10月26日 17时49分
+ * @Copyright 2018 ©Suixingpay. All rights reserved.
+ * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
+ */
+public class testcontroller {
+}
