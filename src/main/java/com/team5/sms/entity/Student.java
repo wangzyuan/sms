f38e6384aae212e5eddcf8c805c71f5ecef433a2
starnightx7 <starnightx7@.163.com>
2018-10-26 18:09:36 +0800

    添加student

--- src/main/java/com/team5/sms/entity/Student.java
+++ src/main/java/com/team5/sms/entity/Student.java
@@ -1,5 +1,7 @@
 package com.team5.sms.entity;
 
+import java.util.Date;
+
 /**
  * All rights Reserved, Designed By Suixingpay.
  *
@@ -9,5 +11,91 @@ package com.team5.sms.entity;
  * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
  */
 public class Student {
+    private String name;
+    private Integer sex;
+    private Integer age;
+    private String sclass;
+    private String teacher;
+    private Date create_time;
+    private String year;
+    private Integer status;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSclass() {
+        return sclass;
+    }
+
+    public void setSclass(String sclass) {
+        this.sclass = sclass;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", sclass='" + sclass + '\'' +
+                ", teacher='" + teacher + '\'' +
+                ", create_time=" + create_time +
+                ", year='" + year + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
