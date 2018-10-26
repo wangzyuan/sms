4eacb4193ac7999d02ce64ce9c4a0d9d66b5119c
starnightx7 <starnightx7@.163.com>
2018-10-26 18:20:49 +0800

    添加controller

--- src/main/java/com/team5/sms/controller/testcontroller.java
+++ src/main/java/com/team5/sms/controller/testcontroller.java
@@ -1,5 +1,8 @@
 package com.team5.sms.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * All rights Reserved, Designed By Suixingpay.
  *
@@ -8,5 +11,11 @@ package com.team5.sms.controller;
  * @Copyright 2018 ©Suixingpay. All rights reserved.
  * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
  */
+@Controller
 public class testcontroller {
+
+    @RequestMapping("/index")
+    public  String index(){
+        return "index";
+    }
 }
